542ee17bee14a33b66608c2f38ab8e070b240fca
Stephen Rowe <stephenrowe@steve-ro.local>
2016-06-16 15:23:51 -0700

    Convert boolean dummies to integer

--- dwe1.R
+++ dwe1.R
@@ -1,5 +1,7 @@
 library('dplyr')
 
+setwd("~/Documents/Springboard/Foundations of Data Science")
+
 # standardize company names
 cleanCompany <- function(company) {
   if (startsWith(company, 'p') | startsWith(company, 'P') | startsWith(company, 'f')) {
@@ -30,6 +32,15 @@ addCategory <- function(code) {
   )
 }
 
+# convert boolean to integer
+boolToInteger <- function(bool) {
+  if(bool) {
+    1
+  } else {
+    0
+  }
+}
+
 # 0. Load the data. Funny that an exercise in R would start with step 0 instead of 1.
 refine_cleaned <- read.table("refine_original.csv", header=TRUE, sep=",")
 
@@ -52,6 +63,14 @@ refine_cleaned$full_address <- paste(refine_cleaned$address, refine_cleaned$city
 # "5: Create dummy variables for company and product category", whatever that means. Using booleans instead of integers. Duh.
 refine_cleaned <- mutate(refine_cleaned, company_philips=(company=='phillips'), company_akzo=(company=='azko'), company_van_houten=(company=='van houten'), company_unilever=(company=='unilever'))
 refine_cleaned <- mutate(refine_cleaned, product_smartphone=(category=='Smartphone'), product_tv=(category=='TV'), product_laptop=(category=='Laptop'), product_tablet=(category=='Tablet'))
+refine_cleaned$company_philips <- sapply(refine_cleaned$company_philips, boolToInteger)
+refine_cleaned$company_akzo <- sapply(refine_cleaned$company_akzo, boolToInteger)
+refine_cleaned$company_van_houten <- sapply(refine_cleaned$company_van_houten, boolToInteger)
+refine_cleaned$company_unilever <- sapply(refine_cleaned$company_unilever, boolToInteger)
+refine_cleaned$product_smartphone <- sapply(refine_cleaned$product_smartphone, boolToInteger)
+refine_cleaned$product_tv <- sapply(refine_cleaned$product_tv, boolToInteger)
+refine_cleaned$product_laptop <- sapply(refine_cleaned$product_laptop, boolToInteger)
+refine_cleaned$product_tablet <- sapply(refine_cleaned$product_tablet, boolToInteger)
 
 # Write it
 write.table(refine_cleaned, file = 'refine_cleaned.csv', quote=FALSE, sep=',', row.names=FALSE)
